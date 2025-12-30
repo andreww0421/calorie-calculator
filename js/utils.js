e7858c6157178ca0c6529ce1e5f8aa34603c0f3b
andreww0421 <jtwen12345us@gmail.com>
2025-12-30 10:37:48 +0800

    Delete js directory

--- js/utils.js
+++
@@ -1,8 +0,0 @@
-function toBase64(file) {
-    return new Promise((r, j) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => r(reader.result.split(',')[1]);
-        reader.onerror = j;
-    });
-}
