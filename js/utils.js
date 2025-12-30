313a65020374db02e357d030e1495a4c4fa9b74f
andreww0421 <jtwen12345us@gmail.com>
2025-12-30 10:14:32 +0800

    Add toBase64 function to convert files to base64

---
+++ js/utils.js
@@ -0,0 +1,8 @@
+function toBase64(file) {
+    return new Promise((r, j) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => r(reader.result.split(',')[1]);
+        reader.onerror = j;
+    });
+}
